ecb60e8aeae3381760227684e57ce6b8da0c7430
James Phillips <james.g.phillips.91@gmail.com>
2024-06-04 22:19:42 -0700

    sketch fix for short term

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ import pb from "../../lib/pocketbase";
 export default async function Home() {
   const posts = await pb.collection('post').getFullList(200 /* batch size */, {
     sort: '-created',
-});;
+  });
+  if (!posts) {
+    <>oof</>
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className="text-white">hello world</h2>
