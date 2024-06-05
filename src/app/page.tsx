48bd7fcc2802c65bee65b0af79bf36904b0796e1
James Phillips <james.g.phillips.91@gmail.com>
2024-06-04 23:11:36 -0700

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 "use server"
-
 import Image from "next/image";
 import pb from "../../lib/pocketbase";
+
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const posts = await pb.collection('post').getFullList(200 /* batch size */, {
     sort: '-created',
   });
-  if (!posts) {
-    <>oof</>
-  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className="text-white">hello world</h2>
