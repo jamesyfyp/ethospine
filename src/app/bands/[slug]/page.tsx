a3ae41725a736573699735e2df52d38c5a4f5ed3
Grady 3 <James@Ashlais-Air.lan>
2024-06-08 18:46:47 -0700

    set up band products

--- src/app/bands/[slug]/page.tsx
+++ src/app/bands/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import pb from "../../../../lib/pocketbase";
 
-
+const pbUrl = process.env.NEXT_PUBLIC_PB_URL
 
 export async function generateStaticParams() {
     const bands = await pb.collection('band').getFullList(200 /* batch size */, {
-        sort: '-created',
-      });
+      sort: '-created',
+    });
    
     return bands.map((band) => ({
       slug: band.slug,
@@ -13,5 +13,30 @@ export async function generateStaticParams() {
   }
 
 export default async function Page({ params }: { params: { slug: string } }) {
-    return <div>My Post: {params.slug}</div>
+  const bandInfo = await pb.collection('band').getFirstListItem(`name~"${params.slug}"`, {
+    expand: 'product'
+  });
+  if (!bandInfo.expand) {
+    return <>error</>
   }
+ 
+  return (
+    <div className="flex-col-center w-full">
+      <h1 className="w-full text-center font-bold text-6xl p-10 ">{bandInfo.name}</h1>
+      <div className="flex columns-2 gap-4 p-10">
+        {bandInfo.expand.product.map((product: any) => {
+          return (
+            <div className="rows-3 justify-center text-center">
+              <img className="w-48" src={`${pbUrl}api/files/${product.collectionId}/${product.id}/${product.images[1]}?thumb=200x200`}/>
+              <h2>{product.name}</h2>
+              <p>{product.type}</p>
+              <p>{product.price}</p>
+              <button> buy </button>
+            </ div>
+          )
+        })}
+      </div>
+      
+    </div>
+  )
+}
