5fff1d1e54c8b0106883b41924d47f208cf99cc2
Grady 3 <James@Ashlais-Air.lan>
2024-07-02 19:54:15 -0700

    try this

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useContext } from "react";
 import CartItem from "./cartItem";
-import Checkout from "./Checkout";
+import Checkout from "./checkout";
 import { CartContext, Product } from "@/contexts/cartContext";
 
 
